6dc5bd2a21ac917237efde62036e424e533c3b56
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-11 16:01:07 +0200

    RPG Exercise

---
+++ RPG/Player.cs
@@ -0,0 +1,18 @@
+﻿
+
+public class Player
+{
+    public int Health;
+
+    public void Attack(Monster target)
+    {
+        target.Health--;
+        Console.WriteLine($"You attack the monster. it has {target.Health} Health");
+    }
+
+    public void Heal()
+    {
+        this.Health = 3;
+        Console.WriteLine($"You healed yourself. you now have {this.Health} Health");
+    }
+}
---
+++ RPG/Program.cs
@@ -0,0 +1,59 @@
+﻿//start
+Player player = new Player();
+player.Health = 3;
+
+Monster monster = new Monster();
+monster.Health = 3;
+
+
+//Game Stats
+Console.WriteLine($@"You have:{player.Health} Health.
+You are fighting a monster with {monster.Health} Health");
+
+
+//Players Action
+while (player.Health > 0)
+{
+    char userInput = GetUserInput();
+    if (userInput == 'A')
+        
+    {
+        player.Attack(monster);
+    }
+    
+    else if (userInput == 'H')
+    {
+      player.Heal();  
+    }
+
+    //Monsters Action
+    if (monster.Health > 0)
+    {
+        monster.Attack(player);
+    }
+    else
+    {
+        monster = new Monster();
+        monster.Health = 3;
+        Console.WriteLine($"A new monster spawns with {monster.Health} Health");
+        
+    }
+}
+monster.Attack(player);
+
+//Player input
+
+Console.WriteLine("GAME OVER");
+
+char GetUserInput()
+{
+    Console.WriteLine($"Do you want to [A]ttack or [H]eal");
+    
+    ConsoleKeyInfo key = Console.ReadKey();
+    
+    Console.WriteLine();
+
+    return char.ToUpper(key.KeyChar);
+}
+   
+
