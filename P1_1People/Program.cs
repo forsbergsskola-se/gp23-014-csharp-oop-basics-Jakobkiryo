0d9656010bf81d0823e4a6cdade964d5e9563cad
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-11 11:58:26 +0200

    Finished Exercise 1

--- P1_1People/Program.cs
+++ P1_1People/Program.cs
@@ -1,8 +1,12 @@
 ﻿
 person[] people = new person[3];
 
+for (int i = 0; i < people.Length; i++)
+{
+    people[i] = new person();
+}
+
 for (var i = 0; i < people.Length; i++)
-    
 {
     Console.WriteLine(people[i]);
 }
