98cb40f320e17aa6ef4132dfe52b080d40346f77
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-11 09:57:17 +0200

    Renamed P1

---
+++ P1Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using P1Classes;
+
+{
+
+    Animal animal;
+    animal = new Animal();
+    Console.WriteLine(animal);
+
+}
+
+{
+    Person person;
+    person = new Person();
+    Console.WriteLine(person);
+}
+
+{
+    Car car;
+    car = new Car();
+    Console.WriteLine(car);
+}
+
+
