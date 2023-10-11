706154b386d5ea3810d93dca4e00b2a71f1830c2
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-11 14:15:40 +0200

    Implement P3_2Person

---
+++ P3_2People/Program.cs
@@ -0,0 +1,14 @@
+﻿person[] person = new person[3];
+
+for (var i = 0; i < person.Length; i++)
+{
+    person[i] = new person();
+    person[i].Name = Console.ReadLine();
+    
+}
+
+for (var i = 0; i < person.Length; i++)
+{
+    person[i].Greeting();
+    
+}
---
+++ P3_2People/person.cs
@@ -0,0 +1,10 @@
+﻿
+public class person
+{
+    public string Name;
+
+    public void Greeting()
+    {
+        Console.WriteLine($"Hello,I am {this.Name} !");
+    }
+}
