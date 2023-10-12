5283a339198794df189a3909563839b1e3bf645d
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-12 13:27:54 +0200

    P4Attack Exercise

--- P4Attack/Player.cs
+++ P4Attack/Player.cs
@@ -2,4 +2,9 @@
 public class Player
 {
     public int damage;
+
+    public void Attack(Player target)
+    {
+        target.damage++; 
+    }
 }
--- P4Attack/Program.cs
+++ P4Attack/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿Player player1 = new Player();
+Player player2 = new Player();
+
+player1.Attack(player2);
+
+Console.WriteLine("Player 1 attacks: Player 2");
+Console.WriteLine("Player 1 DMG:" + player1.damage);
+Console.WriteLine("Player 2 DMG:" + player2.damage);
+
+player2.Attack(player1);
+
+Console.WriteLine("Player 2 attacks: Player 1");
+Console.WriteLine("Player 1 DMG:" + player1.damage);
+Console.WriteLine("Player 2 DMG:" + player2.damage);
+
+
+player1.Attack(player1);
+
+Console.WriteLine("Player 1 Attacks: Player 1");
+Console.WriteLine("Player 1 DMG:" + player1.damage);
+Console.WriteLine("Player 2 DMG:" + player2.damage);
+
+Player player3 = player1;
+
+player2.Attack(player3);
+
+Console.WriteLine("Player 2 Attacks: player 1");
+Console.WriteLine("Player 1 DMG:" + player1.damage);
+Console.WriteLine("Player 2 DMG:" + player2.damage);
+Console.WriteLine("Player 3 DMG:" + player3.damage);
+
