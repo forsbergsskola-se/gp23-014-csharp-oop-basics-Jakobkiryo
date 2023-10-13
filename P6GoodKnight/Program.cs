e241259acfc11b1a6a26b4bbdffaba802483d0f2
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-13 12:40:55 +0200

    Tried to explain the code for myself :)

--- P6GoodKnight/Program.cs
+++ P6GoodKnight/Program.cs
@@ -1,16 +1,19 @@
 ﻿
-{
-
+{ 
+    //Spawns 2 obejcts/knights one attacker and one defender
     Knight Attacker = new Knight();
     Knight Defender = new Knight();
-
+    
+    //Spawns a shield for the knight
     Defender.Shield = new Shield();
     
     Attacker.Attack(Defender);
-
+    
+    //Spawns a new sword
     Attacker.Sword = new Sword();
     Console.WriteLine("Equipping Sword...");
     
+    //The attacking knight attacks the defender with different outcome
     Attacker.Attack(Defender);
     Attacker.Attack(Defender);
 }
